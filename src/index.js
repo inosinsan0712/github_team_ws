a801fdeb1211e4c653e038c363667d3c8abf42c0
井上慎太郎 <inoueshintarou@inoueshintarounoMacBook-Air.local>
2021-02-17 22:07:41 +0900

    <get>

--- src/index.js
+++ src/index.js
@@ -133,6 +133,19 @@
 
 	/* TODO: 演習2 以降は下記に自由に記入 */
 
+	Function mul(){
+		eqlNum.value = parseFloat(midNum[0].value)*parseFloat(midNum[1].value);
+		}
+		
+		
+		Function div(){
+		if(parseFloat(midNum[1].value) == 0){ eqlNum.value = “ERROR”;
+		}else{
+		eqlNum.value = parseFloat(midNum[0].value)/parseFloat(midNum[1].value);
+		}
+		}
+		
+
 	/* 演習の編集範囲 おわり */
 
 	// ac を押したときの処理
